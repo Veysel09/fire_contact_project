0e149fdb006cc3621adf8f33485cc966cb1e6427
Veysel Aydin <weycell.aydin09@gmail.com>
2023-03-07 20:26:53 +0300

    formComp. added to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Contact from "./components/contacts/Contacts";
+import FormCoponents from "./components/form/FormCoponents";
 
 function App() {
   return (
     <div className="App">
       <Contact />
+      <FormCoponents />
     </div>
   );
 }
