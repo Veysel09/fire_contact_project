bda6226b9b430b8f4e39cd34ce990581f44fed30
Veysel Aydin <weycell.aydin09@gmail.com>
2023-03-07 20:24:43 +0300

    app.js added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Contact from "./components/contacts/Contacts";
 
 function App() {
-  return <div className="App">erf</div>;
+  return (
+    <div className="App">
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
