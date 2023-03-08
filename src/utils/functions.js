398790cc0733e8f679d898512033dd767a64cd16
Veysel Aydin <weycell.aydin09@gmail.com>
2023-03-08 13:42:02 +0300

    useFetch

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,6 +1,7 @@
 // AddUser
 
-import { getDatabase, ref, set, push } from "firebase/database";
+import { getDatabase, ref, set, push, onValue} from "firebase/database";
+import { useEffect, useState } from "react";
 import firebase from "./firebase";
 
 export const AddUser = (info) => {
@@ -14,4 +15,29 @@ export const AddUser = (info) => {
     gender: info.gender,
   });
   console.log("addUser a bilgiler kaydedildi");
+};
+
+//Read Info
+
+export const useFetch = () => {
+   
+    const [contactList, setContactList]= useState()
+    const [isLoading, setIsLoading] = useState()
+
+  useEffect(() => {
+    const db = getDatabase(firebase);
+    const userRef = ref(db, "users/");
+
+    onValue(userRef, (snapshot) =>{
+        const data = snapshot.val()
+        const userArray = []
+
+        for(let id in data ){
+            userArray.push({id,...data[id]})
+        }
+        setContactList(userArray)
+        setIsLoading(false)
+    })
+    return {isLoading, contactList}
+  }, []);
 };
