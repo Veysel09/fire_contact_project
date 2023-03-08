8847ffa7d0b4da48b37765def05f76c923f52c17
Veysel Aydin <weycell.aydin09@gmail.com>
2023-03-08 10:48:50 +0300

    initialValues

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 import Contact from "./components/contacts/Contacts";
 import FormCoponents from "./components/form/FormCoponents";
 
+const initialValues = {
+  username: "",
+  phoneNumber: "",
+  gender: "", 
+};
+
 function App() {
+  const [info, setInfo] = useState(initialValues);
+
   return (
     <div className="App">
-      <FormCoponents />
+      <FormCoponents info ={info} setInfo={setInfo} />
       <Contact />
     </div>
   );
